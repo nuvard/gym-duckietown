7f4f46b501de2117262d941121cb115c7a53b09f
nuvard <nikolskaya2000@yandex.ru>
2020-07-07 17:58:30 +0000

    Added performance animation with colab support

--- learning/imitation/pytorch/enjoy_imitation.py
+++ learning/imitation/pytorch/enjoy_imitation.py
@@ -7,18 +7,25 @@ learning, and visualize the result.
 
 import time
 import sys
+sys.path.insert(0, '..')
+sys.path.insert(0, '../..')
+sys.path.append('/content/gym-duckietown/src')
+
 import argparse
 import math
-
+from IPython import display as ipythondisplay
 import torch
 
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
+from IPython import display as ipythondisplay
 
 from utils.env import launch_env
 from utils.wrappers import NormalizeWrapper, ImgWrapper, \
     DtRewardWrapper, ActionWrapper, ResizeWrapper
 from utils.teacher import PurePursuitExpert
+
 
 from imitation.pytorch.model import Model
 
@@ -28,7 +35,7 @@ def _enjoy():
     model = Model(action_dim=2, max_action=1.)
 
     try:
-        state_dict = torch.load('trained_models/imitate.pt', map_location=device)
+        state_dict = torch.load('models/imitate.pt', map_location=device)
         model.load_state_dict(state_dict)
     except:
         print('failed to load model')
@@ -44,7 +51,7 @@ def _enjoy():
     env = DtRewardWrapper(env)
 
     obs = env.reset()
-
+    i=0
     while True:
         obs = torch.from_numpy(obs).float().to(device).unsqueeze(0)
 
@@ -52,15 +59,22 @@ def _enjoy():
         action = action.squeeze().data.cpu().numpy()
         
         obs, reward, done, info = env.step(action)
-        env.render()
-
+        prev_screen = env.render(mode='rgb_array')
+        plt.imshow(prev_screen)
+        plt.savefig(f"figs/Fig{i}.png")
+        i+=1
+        ipythondisplay.clear_output(wait=True)
+        ipythondisplay.display(plt.gcf())
         if done:
             if reward < 0:
                 print('*** FAILED ***')
                 time.sleep(0.7)
                 
             obs = env.reset()
-            env.render()
+            env.render(mode='rgb_array')
 
+def colab_enjoy():
+  _enjoy()
+  
 if __name__ == '__main__':
     _enjoy()
